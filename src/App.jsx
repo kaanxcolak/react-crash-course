25a59e368aaf3f89f7a589078580cd50a59dcbe8
Kaan Çolak <kaancolak3458@gmail.com>
2024-01-02 23:29:46 +0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Post from './components/Post';
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return <Post / >
+}
+
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+    return (
+<div>
+    <p>Kaan</p>
+    <p>ReactJS is a super!</p>
+</div>
+    );
+}
+
+export default Post;
