229367091d228fa48a1617b36df6f7602adb9b96
Kaan Çolak <kaancolak3458@gmail.com>
2024-01-15 03:20:30 +0300

    updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,11 @@
 const names = ['Kaan','Colak'];
 
-function Post() {
+function Post(props) {
     const chosenName = Math.random() > 0.5 ? names[0] : names[1];
     return (
 <div>
-    <p>{chosenName}</p>
-    <p>ReactJS is a super!</p>
+    <p>{props.author}</p>
+    <p>{props.body}</p>
 </div>
     );
 }
